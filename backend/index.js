4c44409b7cab01d5f7dd5ff22cd05cff126c2f37
ManojRDhundhalva <manojrdhundhalva@gmail.com>
2024-07-06 18:24:24 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Import Routes
 const loginRoutes = require("./routes/login");
 const registerRoutes = require("./routes/register");
 const profileRoutes = require("./routes/profile");
@@ -6,33 +15,29 @@ const bankDetailsRoutes = require("./routes/bankDetails");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
 const wishListRoutes = require("./routes/wishList");
-const { verifyTokenAndAuthorizationUser } = require("./middlewares/verifyUser");
 const orderRoutes = require("./routes/order");
 const inventoryRoutes = require("./routes/inventory");
 const shipperRoutes = require("./routes/shipper");
 const categoryRoutes = require("./routes/category");
 const databaseRoutes = require("./routes/database");
 const verifyEmail = require("./routes/verifyEmail");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const { verifyTokenAndAuthorizationUser } = require("./middlewares/verifyUser");
 
-dotenv.config();
-const app = express();
-const port = process.env.PORT || 8000;
-
+// Middleware
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "authorization"],
     credentials: true,
   })
 );
 
 app.use(express.json());
 
-app.get("/", (req, resp) => {
-  resp.send("hello");
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.use("/login", loginRoutes);
@@ -49,13 +54,14 @@ app.use("/category", categoryRoutes);
 app.use("/database", databaseRoutes);
 app.use("/verify-email", verifyEmail);
 
-app.get("/getTomTomApiKey", verifyTokenAndAuthorizationUser, (_, res) => {
-  return res.status(200).json({
-    message: "Api key sent successfully!",
+app.get("/getTomTomApiKey", verifyTokenAndAuthorizationUser, (req, res) => {
+  res.status(200).json({
+    message: "API key sent successfully!",
     apiKey: process.env.TOMTOM_API_KEY,
   });
 });
 
+// Start Server
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
